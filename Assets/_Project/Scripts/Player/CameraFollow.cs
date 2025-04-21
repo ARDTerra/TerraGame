c398a82f230eebe09e6348d2e1e6983fbe2d246d
ARDTerra <83560570+ARDTerra@users.noreply.github.com>
2025-04-21 20:36:57 +0930

    Movement
    Movement and game set up

---
+++ Assets/_Project/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothTime;
+    public Vector3 offsetPos;
+
+    public Vector3 offsetRot;
+    public float rotationSpeed;
+
+    private Vector3 velocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target != null)
+        {
+            Vector3 targetPosition = target.position + offsetPos;
+
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+            transform.rotation = Quaternion.Euler(offsetRot * rotationSpeed);
+        }
+    }
+}
